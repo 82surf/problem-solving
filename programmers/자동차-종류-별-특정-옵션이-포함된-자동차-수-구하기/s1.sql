63fb612c50247af6de8a9c3111a03b9b8f3f675c
82surf <82surf@gmail.com>
2023-03-30 14:18:35 +0900

    solve: programmers | 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기 | sql

---
+++ programmers/자동차-종류-별-특정-옵션이-포함된-자동차-수-구하기/s1.sql
@@ -0,0 +1,12 @@
+-- 옵션이 포함된 자동차
+-- 자동차 종류 별로 group
+-- count
+
+SELECT A.CAR_TYPE, COUNT(*) AS CARS
+FROM (
+    SELECT CAR_TYPE
+    FROM CAR_RENTAL_COMPANY_CAR
+    WHERE OPTIONS LIKE '%가죽시트%' OR OPTIONS LIKE '%열선시트%' OR OPTIONS LIKE '%통풍시트%'
+) AS A
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE;
