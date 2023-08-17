ced60f0b87ac5d7a3448e26738be741826dd6c6c
82surf <82surf@gmail.com>
2023-08-17 09:31:11 +0900

    solve: BOJ | 1546 | python

---
+++ BOJ/1546/s1.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input.txt')
+
+N = int(input())
+scores = list(map(int, input().split()))
+M = max(scores)
+
+total = 0
+for score in scores:
+    total += score / M * 100
+print(total / N)
