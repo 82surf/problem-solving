447a43fec20f906bb75c59626dde676c62b6e12c
82surf <82surf@gmail.com>
2023-08-22 15:24:35 +0900

    solve: programmers | 조건별로 분류하여 주문상태 출력하기 | sql

---
+++ programmers/조건별로-분류하여-주문상태-출력하기/s1.sql
@@ -0,0 +1,12 @@
+-- 5월 1일까지: 출고완료
+-- 이후: 출고대기
+-- 미정: 출고미정
+
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE, '%Y-%m-%d') AS OUT_DATE,
+    CASE
+        WHEN OUT_DATE <= '2022-05-01' THEN '출고완료'
+        WHEN OUT_DATE IS NULL THEN '출고미정'
+        ELSE '출고대기'
+    END AS 출고여부
+FROM FOOD_ORDER
+ORDER BY ORDER_ID;
