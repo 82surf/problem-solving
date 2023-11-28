c7ec796dba074106efa87883bf1f59dbfaec1332
82surf <82surf@gmail.com>
2023-11-28 19:30:08 +0900

    solve: programmers | 최솟값 만들기 | java

--- programmers/java/src/P12941.java
+++ programmers/java/src/P12941.java
@@ -4,15 +4,11 @@ import java.util.Collections;
 public class P12941 {
     public int solution(int[] A, int[] B) {
         Arrays.sort(A);
-
-        Integer[] integerB = Arrays.stream(B).boxed().toArray(Integer[]::new);
-        Arrays.sort(integerB, Collections.reverseOrder());
-
-        int[] descB = Arrays.stream(integerB).mapToInt(i -> i).toArray();
+        Arrays.sort(B);
 
         int answer = 0;
         for (int i = 0; i < A.length; i++) {
-            answer += A[i] * descB[i];
+            answer += A[i] * B[B.length - i - 1];
         }
         return answer;
     }
